d6e91355c4de354cca8b7bf2d7b5e1cafd10ec07
LuisRafaFlores <aikidoxalapa@gmail.com>
2018-10-15 11:46:09 -0500

    mi primer commit en la nueva rama

---
+++ src/holamundogit/ClaseNuevaRama.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundogit;
+
+/**
+ *
+ * @author psicf
+ */
+public class ClaseNuevaRama {
+
+    public ClaseNuevaRama() {
+    }
+
+    @Override
+    public String toString() {
+        return "ClaseNuevaRama{" + '}';
+    }
+    
+}
